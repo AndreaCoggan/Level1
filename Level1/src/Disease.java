759cd103e5296f3c07c81c2bb65096bdf7a8b570
AndreaCoggan <andrealcoggan@yahoo.com>
2015-08-14 18:31:21 -0700

    8.14.15
    setters and getters

---
+++ Level1/src/Disease.java
@@ -0,0 +1,19 @@
+
+public class Disease {
+	
+		boolean isPandemic;
+		String Symptoms;
+		float chanceOfSurvival;
+		
+		
+		
+	Disease(boolean isPandemic,String Symptoms, float chanceOfSurvival){
+		this.isPandemic = isPandemic;
+		this.Symptoms = Symptoms;
+		this.chanceOfSurvival = chanceOfSurvival;
+	}
+	Disease sneezing= new Symptom();
+	Disease coughing= new Symptom();
+	  
+	
+}
