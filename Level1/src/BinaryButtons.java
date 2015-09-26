fe872b1079f201c6ee66bec9341d71e5f6e73757
AndreaCoggan <andrealcoggan@yahoo.com>
2015-09-25 18:28:24 -0700

    Binary game and popcorn and pig latin

--- Level1/src/BinaryButtons.java
+++ Level1/src/BinaryButtons.java
@@ -1,5 +1,10 @@
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
+
+import javax.print.DocFlavor.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -40,8 +45,13 @@ public class BinaryButtons implements ActionListener {
 		button4.setText("");
 		button5.setText("");
 		clearButton.setText("clear");
+		button1.setIcon(createImage("blank_card.png"));
+		button2.setIcon(createImage("blank_card.png"));
+		button3.setIcon(createImage("blank_card.png"));
+		button4.setIcon(createImage("blank_card.png"));
+		button5.setIcon(createImage("blank_card.png"));
 
-		frame.setSize(800, 100);
+		frame.setSize(1500, 400);
 		button1.addActionListener((ActionListener) this);
 		button2.addActionListener((ActionListener) this);
 		button3.addActionListener((ActionListener) this);
@@ -50,51 +60,68 @@ public class BinaryButtons implements ActionListener {
 		clearButton.addActionListener((ActionListener) this);
 	}
 
+	private Icon createImage(String filename) {
+		java.net.URL imageURL = getClass().getResource(filename);
+		Icon icon = new ImageIcon(imageURL);
+		return icon;
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton clickedButton = (JButton) e.getSource();
 
 		if (clickedButton.equals(button1)) {
 			if (button1.getText().equals("")) {
-				button1.setText("16");
+				button1.setIcon(createImage("16_card.png"));
+				button1.setText(" ");
 				sum = sum + 16;
 			} else {
+				button1.setIcon(createImage("blank_card.png"));
 				button1.setText("");
 				sum = sum - 16;
 			}
 		}
 		if (clickedButton.equals(button2)) {
 			if (button2.getText().equals("")) {
-				button2.setText("8");
+				button2.setIcon(createImage("8_card.png"));
+				button2.setText(" ");
 				sum = sum + 8;
 			} else {
+				button2.setIcon(createImage("blank_card.png"));
 				button2.setText("");
 				sum = sum - 8;
 			}
 		}
 		if (clickedButton.equals(button3)) {
 			if (button3.getText().equals("")) {
-				button3.setText("4");
+				button3.setIcon(createImage("4_card.png"));
+				button3.setText(" ");
 				sum = sum + 4;
 			} else {
+				button3.setIcon(createImage("blank_card.png"));
 				button3.setText("");
 				sum = sum - 4;
 			}
 		}
 		if (clickedButton.equals(button4)) {
 			if (button4.getText().equals("")) {
-				button4.setText("2");
+				button4.setIcon(createImage("2_card.png"));
+				button4.setText(" ");
 				sum = sum + 2;
 			} else {
+				button4.setIcon(createImage("blank_card.png"));
 				button4.setText("");
 				sum = sum - 2;
 			}
 		}
 		if (clickedButton.equals(button5)) {
 			if (button5.getText().equals("")) {
-				button5.setText("1");
+				button5.setIcon(createImage("one_card.png"));
+				button5.setText(" ");
 				sum = sum + 1;
 			} else {
+				button5.setIcon(createImage("blank_card.png"));
 				button5.setText("");
 				sum = sum - 1;
 			}
